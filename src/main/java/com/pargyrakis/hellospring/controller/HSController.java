c58fc0450b55b1412ff1b260d0c12da2331d4b17
panargirakis <panargirakis@gmail.com>
2020-10-04 16:22:49 -0400

    added dependency injection and content repository

--- src/main/java/com/pargyrakis/hellospring/controller/HSController.java
+++ src/main/java/com/pargyrakis/hellospring/controller/HSController.java
@@ -1,12 +1,27 @@
 package com.pargyrakis.hellospring.controller;
 
+import com.pargyrakis.hellospring.data.GifRepository;
+import com.pargyrakis.hellospring.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HSController {
+
+    @Autowired  // dependency injection
+    private GifRepository gifRepository;
+
     @RequestMapping
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
